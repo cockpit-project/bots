6d55ccdf0d36197571a180441416e08c7ace7466
Martin Pitt <martin@piware.de>
2019-09-30 16:25:16 +0200

    images: Use rhel-7.8's spec file for rhel/centos-7 images
    master's cockpit.spec grew some stricter version constraints than what's
    available on RHEL 7. Due to yum silently ignoring unavailable versions,
    the mock was missing systemd-devel.
    
    Use the rhel-7.8 branch for building the RHEL 7 based images instead,
    which also gets rid of the previous golang hardcoding.
    
    Fixes #41
    Closes #58

--- images/scripts/lib/build-deps.sh
+++ images/scripts/lib/build-deps.sh
@@ -2,15 +2,16 @@
 
 set -eu
 
+# most images use cockpit.spec from  master branch
+case "$1" in
+    rhel*7*|centos*7*) branch=rhel-7.8 ;;
+    *) branch=master ;;
+esac
+
 # Download cockpit.spec, replace `npm-version` macro and then query all build requires
-curl -s https://raw.githubusercontent.com/cockpit-project/cockpit/master/tools/cockpit.spec |
+curl -s https://raw.githubusercontent.com/cockpit-project/cockpit/$branch/tools/cockpit.spec |
     sed 's/%{npm-version:.*}/0/' |
     sed '/Recommends:/d' |
     rpmspec -D "$1" --buildrequires --query /dev/stdin |
     sed 's/.*/"&"/' |
     tr '\n' ' '
-
-# support for backbranches
-if [ "$1" = "rhel 7" ] || [ "$1" = "centos 7" ]; then
-    echo "golang-bin golang-src"
-fi
