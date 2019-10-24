3bd26fdd26f2e9ceaf3fba165e29583f8ef0c07b
Martin Pitt <martin@piware.de>
2019-10-24 09:12:23 +0200

    images: Add back intltool to rhel-8* image mocks
    Cockpit master dropped intltool recently [1], but the stable
    rhel-8.{0,1} branches don't (and shouldn't) have that commit. Put it
    back for now so that we can build them.
    
    This can be dropped when obsoleting rhel-8.{0,1} branches and images.
    
    [1] https://github.com/cockpit-project/cockpit/pull/12979

--- images/scripts/lib/build-deps.sh
+++ images/scripts/lib/build-deps.sh
@@ -3,9 +3,13 @@
 set -eu
 
 # most images use cockpit.spec from  master branch
+branch="master"
+stable_branch_deps=""
+
 case "$1" in
-    rhel*7*|centos*7*) branch=rhel-7.8 ;;
-    *) branch=master ;;
+    rhel*7|centos*7) branch=rhel-7.8 ;;
+    # intltool got removed on cockpit master, but not from rhel-8.{0,1} branch
+    rhel*8) stable_branch_deps="intltool" ;;
 esac
 
 # Download cockpit.spec, replace `npm-version` macro and then query all build requires
@@ -15,3 +19,5 @@ curl -s https://raw.githubusercontent.com/cockpit-project/cockpit/$branch/tools/
     rpmspec -D "$1" --buildrequires --query /dev/stdin |
     sed 's/.*/"&"/' |
     tr '\n' ' '
+
+echo "$stable_branch_deps"
