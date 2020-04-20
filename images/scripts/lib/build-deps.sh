551c84ac22e064df113d4af100ec63b844e5f418
Matej Marusak <mmarusak@redhat.com>
2020-04-20 09:26:47 +0200

    Drop Fedora 30

--- images/scripts/lib/build-deps.sh
+++ images/scripts/lib/build-deps.sh
@@ -9,7 +9,7 @@ stable_branch_deps=""
 case "$1" in
     rhel*7|centos*7) branch=rhel-7.8 ;;
     # intltool got removed on cockpit master, but not from rhel-[78].* branches
-    rhel*8|fedora*30|fedora*31) stable_branch_deps="intltool" ;;
+    rhel*8|fedora*31) stable_branch_deps="intltool" ;;
 esac
 
 # Download cockpit.spec, replace `npm-version` macro and then query all build requires
