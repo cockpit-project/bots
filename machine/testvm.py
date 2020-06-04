8a96eba0c50f80a354dae2b580a5fd2ed5dddceb
Martin Pitt <martin@piware.de>
2020-06-04 10:25:01 +0200

    Fix BOTS_DIR imports
    This is defined in machine_core.constants. machine_core.directories just
    happens to import it for itself, but it's soon not going to do that any
    more.

--- machine/testvm.py
+++ machine/testvm.py
@@ -29,7 +29,7 @@ from machine_core.timeout import Timeout
 from machine_core.machine import Machine
 from machine_core.exceptions import Failure, RepeatableFailure
 from machine_core.machine_virtual import VirtMachine, VirtNetwork
-from machine_core.constants import BOTS_DIR, TEST_DIR, IMAGES_DIR, SCRIPTS_DIR, DEFAULT_IMAGE, TEST_OS_DEFAULT
+from machine_core.constants import BASE_DIR, BOTS_DIR, TEST_DIR, IMAGES_DIR, SCRIPTS_DIR, DEFAULT_IMAGE, TEST_OS_DEFAULT
 from machine_core.cli import cmd_cli
 from machine_core.directories import get_images_data_dir, get_temp_dir
 from task.testmap import get_build_image, get_test_image
