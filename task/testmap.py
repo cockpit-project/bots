9f63d1bffdeab9afb47f72fa13d41566d86b8a47
Martin Pitt <martin@piware.de>
2021-01-05 17:06:51 +0100

    testmap: Declare cockpit rhel-8-4-distropkg test
    Drop the manual rhel-8-3-distropkg, as it's already a part of the master
    tests.

--- task/testmap.py
+++ task/testmap.py
@@ -59,7 +59,7 @@ REPO_BRANCH_CONTEXT = {
         '_manual': [
             'fedora-testing',
             'fedora-testing/dnf-copr',
-            'rhel-8-3-distropkg',
+            'rhel-8-4-distropkg',
         ],
     },
     'cockpit-project/starter-kit': {
