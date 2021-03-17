8342e800dd5a12ac6764dfe32f8e4d326a095282
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2021-03-17 14:34:51 +0100

    s3: add support for v4 authentication
    Add a new method for signing requests according to the AWS4-HMAC-SHA256
    signature scheme.  Add a helper method to format the returned headers in
    a format suitable for use with curl.

--- lib/s3.py
+++ lib/s3.py
@@ -4,22 +4,34 @@
 
 # Adapted from examples in
 # https://s3.amazonaws.com/doc/s3-developer-guide/RESTAuthentication.html
+# and
+# https://docs.aws.amazon.com/AmazonS3/latest/API/sig-v4-authenticating-requests.html
 
 import base64
+import hashlib
 import hmac
 import os.path
 import sys
 import time
 import urllib.parse
+from typing import Dict, List
+
 from .directories import xdg_config_home
 
 __all__ = (
+    "ACL",
     "ACL_PUBLIC",
+    "PUBLIC",
     "is_key_present",
+    "sign_curl",
+    "sign_request",
     "sign_url",
 )
 
-ACL_PUBLIC = 'x-amz-acl:public-read'
+ACL = 'x-amz-acl'
+PUBLIC = 'public-read'
+ACL_PUBLIC = f'{ACL}:{PUBLIC}'
+SHA256_NIL = hashlib.sha256(b'').hexdigest()
 
 
 def get_key(hostname):
@@ -43,6 +55,44 @@ def get_key(hostname):
 def is_key_present(url: urllib.parse.ParseResult) -> bool:
     """Checks if an S3 key is available for the given url"""
     return get_key(url.hostname) is not None
+
+
+def sign_request(url: urllib.parse.ParseResult, method='GET', checksum=SHA256_NIL, headers={}) -> Dict[str, str]:
+    """Signs an AWS request using the AWS4-HMAC-SHA256 algorithm
+
+    Returns a dictionary of extra headers which need to be sent along with the request.
+    If the method is PUT then the checksum of the data to be uploaded must be provided.
+    @headers, if given, are a dict of additional headers to be signed (eg: `x-amz-acl`)
+    """
+    access_key, secret_key = get_key(url.hostname)
+
+    amzdate = time.strftime('%Y%m%dT%H%M%SZ', time.gmtime())
+
+    headers = headers.copy()  # don't modify the user's copy, or the default
+    headers.update({'host': url.hostname, 'x-amz-content-sha256': checksum, 'x-amz-date': amzdate})
+    headers_str = ''.join(f'{k}:{v}\n' for k, v in sorted(headers.items()))
+    headers_list = ';'.join(sorted(headers))
+
+    credential_scope = f'{amzdate[:8]}/any/s3/aws4_request'
+    signing_key = f'AWS4{secret_key}'.encode('ascii')
+    for item in credential_scope.split('/'):
+        signing_key = hmac.new(signing_key, item.encode('ascii'), hashlib.sha256).digest()
+
+    algorithm = 'AWS4-HMAC-SHA256'
+    canonical_request = f'{method}\n{url.path}\n{url.query}\n{headers_str}\n{headers_list}\n{checksum}'
+    request_hash = hashlib.sha256(canonical_request.encode('ascii')).hexdigest()
+    string_to_sign = f'{algorithm}\n{amzdate}\n{credential_scope}\n{request_hash}'
+    signature = hmac.new(signing_key, string_to_sign.encode('ascii'), hashlib.sha256).hexdigest()
+    headers['Authorization'] = f'{algorithm} Credential={access_key}/{credential_scope},' \
+        f'SignedHeaders={headers_list},Signature={signature}'
+
+    return headers
+
+
+def sign_curl(url: urllib.parse.ParseResult, method='GET', checksum=SHA256_NIL, headers={}) -> List[str]:
+    """Same as sign_request() but formats the result as an argument list for curl, including the url"""
+    headers = sign_request(url, method=method, checksum=checksum, headers=headers)
+    return [f'-H{key}:{value}' for key, value in headers.items()] + [url.geturl()]
 
 
 def sign_url(url: urllib.parse.ParseResult, method='GET', headers=[], duration=12 * 60 * 60) -> str:
