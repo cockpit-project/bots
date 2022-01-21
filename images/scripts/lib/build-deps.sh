2815e1ec04a1fab064e9f173063a3af2b8bf20cb
Martin Pitt <martin@piware.de>
2022-01-21 06:28:02 +0100

    images: Adjust build-deps.sh to recent cockpit spec reorg
    [1] changed cockpit's template spec format and location quite
    profoundly. Compute the rpmspec-parseable spec independently for the
    main and rhel-7.9 branches; the latter continues to use the same
    approach as before, for main make use of the new BUILD_ALL template and
    just disable all other templates -- we don't need them for build
    dependencies.
    
    Also fix the curl command to actually fail on 404s and other errors.
    
    [1] https://github.com/cockpit-project/cockpit/pull/16853

--- images/scripts/lib/build-deps.sh
+++ images/scripts/lib/build-deps.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+# Download cockpit.spec, replace `npm-version` macro and then query all build requires
+# also re-enable building of optional packages by default, so that we get libssh-devel
+
 set -eu
 # Guard against GitHub outages, redirects etc., and let this script fail on rpmspec failures
 set -o pipefail
 
-# most images use cockpit.spec from main branch
-branch="main"
+GET="curl --silent --show-error --fail"
+COCKPIT_GIT="https://raw.githubusercontent.com/cockpit-project/cockpit"
+OS_VER="$1"
 
-case "$1" in
-    rhel*7|centos*7) branch=rhel-7.9 ;;
+# most images use cockpit.spec from main branch, but there's a RHEL 7 stable branch with a completely different layout
+case "$OS_VER" in
+    rhel*7|centos*7)
+        spec=$($GET "$COCKPIT_GIT/rhel-7.9/tools/cockpit.spec" |
+            sed 's/%{npm-version:.*}/0/; /Recommends:/d; s/build_optional 0/build_optional 1/')
+        ;;
+
+    *)
+        spec=$($GET "$COCKPIT_GIT/main/tools/cockpit.spec.in" | sed 's/@BUILD_ALL@/1/; s/@[A-Z_]*/0/g')
+        ;;
 esac
 
-# Download cockpit.spec, replace `npm-version` macro and then query all build requires
-# also re-enable building of optional packages by default, so that we get libssh-devel
-curl -s https://raw.githubusercontent.com/cockpit-project/cockpit/$branch/tools/cockpit.spec |
-    sed 's/%{npm-version:.*}/0/; /Recommends:/d; s/build_optional 0/build_optional 1/' |
-    rpmspec -D "$1" --buildrequires --query /dev/stdin |
-    sed 's/.*/"&"/' |
-    tr '\n' ' '
+echo "$spec" | rpmspec -D "$OS_VER" --buildrequires --query /dev/stdin | sed 's/.*/"&"/' | tr '\n' ' '
