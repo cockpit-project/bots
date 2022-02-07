5d127892e6878d0e8fceaf605c857e263d38349a
Martin Pitt <martin@piware.de>
2022-02-07 14:23:11 +0100

    testmap: Current Fedora CoreOS is based on Fedora 35
    Adjust the build image accordingly.

--- lib/testmap.py
+++ lib/testmap.py
@@ -183,7 +183,7 @@ REPO_BRANCH_CONTEXT = {
 # The OSTree variants can't build their own packages, so we build in
 # their non-Atomic siblings.
 OSTREE_BUILD_IMAGE = {
-    "fedora-coreos": "fedora-34",
+    "fedora-coreos": "fedora-35",
 }
 
 # only put auxiliary images here; triggers for primary OS images are computed from testmap
