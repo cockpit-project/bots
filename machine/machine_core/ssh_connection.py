cockpit-project/bots

commit 90c0b2efadede35a9cbc583c61a9dfc450eb7e56

machine_core: Simplify MachineSSHConnection.execute()
Drop the `script=` kwarg and mode. Nothing uses this any more, and it's
redundant as `command` can take a multi-line shell command as well.

Also drop `command` as a kwarg, and use it as first (mandatory)
positional argument. Again, nothing else calls that with a keyword.

Use `subprocess.run()` instead of our hand-rolled complicated way of
communicating with the process. Note that we cannot use `text=True` and
`errors="replace"`, as that mangles line endings (which some tests rely
on).

machine/machine_core/ssh_connection.py
@@ -259,24 +259,19 @@ class SSHConnection(object):
         else:
             return ["-o", "ControlPath=" + self.ssh_master]
 
-    def execute(self, command=None, script=None, input=None, environment={},
+    def execute(self, command, input=None, environment={},
                 stdout=None, quiet=False, direct=False, timeout=120,
                 ssh_env=["env", "-u", "LANGUAGE", "LC_ALL=C"], check=True):
         """Execute a shell command in the test machine and return its output.
 
-        Either specify @command or @script
-
-        Arguments:
-            command: The string to execute by /bin/sh; or
-                     an argument list execute without shell interpretation
-            script: A multi-line script to execute in /bin/sh
+            command: The string or argument list to execute by /bin/sh (still with shell interpretation)
             input: Input to send to the command
             environment: Additional environment variables
             timeout: Applies if not already wrapped in a #Timeout context
         Returns:
-            The command/script output as a string.
+            The command output as a string.
         """
-        assert command or script
+        assert command
         assert self.ssh_address
 
         if not self.__ssh_direct_opt_var(direct=direct):
@@ -306,70 +301,25 @@ class SSHConnection(object):
 
         additional_ssh_params += self.__execution_opts(direct=direct)
 
-        if command:
-            if getattr(command, "strip", None):  # Is this a string?
-                cmd += [command]
-                if not quiet:
-                    self.message("+", command)
-            else:
-                # use shlex.join() once Python 3.8 is available everywhere
-                cmd.append(' '.join(shlex.quote(arg) for arg in command))
-                if not quiet:
-                    self.message("+", *command)
+        if getattr(command, "strip", None):  # Is this a string?
+            cmd += [command]
+            if not quiet:
+                self.message("+", command)
         else:
-            assert not input, "input not supported to script"
-            cmd += ["sh", "-s"]
-            if self.verbose:
-                cmd += ["-x"]
-            input = env_script
-            input += script
-            command = "<script>"
+            # use shlex.join() once Python 3.8 is available everywhere
+            cmd.append(' '.join(shlex.quote(arg) for arg in command))
+            if not quiet:
+                self.message("+", *command)
         command_line = ssh_env + default_ssh_params + additional_ssh_params + env_command + cmd
 
         with timeoutlib.Timeout(seconds=timeout, error_message="Timed out on '%s'" % command, machine=self):
-            if stdout:
-                subprocess.call(command_line, stdout=stdout)
-                return
+            res = subprocess.run(command_line,
+                                 stdin=None if input is not None else subprocess.DEVNULL,
+                                 input=input.encode("UTF-8") if input else None,
+                                 stdout=stdout or subprocess.PIPE,
+                                 check=check)
 
-            output = ""
-            proc = subprocess.Popen(command_line, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdin_fd = proc.stdin.fileno()
-            stdout_fd = proc.stdout.fileno()
-            stderr_fd = proc.stderr.fileno()
-            rset = [stdout_fd, stderr_fd]
-            wset = [stdin_fd]
-            while len(rset) > 0 or len(wset) > 0:
-                ret = select.select(rset, wset, [], 10)
-                for fd in ret[0]:
-                    if fd == stdout_fd:
-                        data = os.read(fd, 1024)
-                        if not data:
-                            rset.remove(stdout_fd)
-                            proc.stdout.close()
-                        else:
-                            if self.verbose:
-                                write_all(sys.__stdout__.fileno(), data)
-                            output += data.decode('utf-8', 'replace')
-                    elif fd == stderr_fd:
-                        data = os.read(fd, 1024)
-                        if not data:
-                            rset.remove(stderr_fd)
-                            proc.stderr.close()
-                        elif not quiet or self.verbose:
-                            write_all(sys.__stderr__.fileno(), data)
-                for fd in ret[1]:
-                    if fd == stdin_fd:
-                        if input:
-                            num = os.write(fd, input.encode('utf-8'))
-                            input = input[num:]
-                        if not input:
-                            wset.remove(stdin_fd)
-                            proc.stdin.close()
-            proc.wait()
-
-        if proc.returncode != 0 and check:
-            raise subprocess.CalledProcessError(proc.returncode, command, output=output)
-        return output
+        return None if stdout else res.stdout.decode("UTF-8", "replace")
 
     def upload(self, sources, dest, relative_dir=TEST_DIR, use_scp=False):
         """Upload a file into the test machine

commit e864a2adb39e8cd314ff0068f5a4135c7eb7490c

machine_core: fix environment handling in .execute()
The changes made in 90c0b2efadede35a9cbc583c61a9dfc450eb7e56 introduced
a couple of bugs.

 - They removed the use of `env_script` without removing the code to
   compute it.

 - The environment variables were being prepended as `KEY=VAL` arguments
   before the command to be run.  The introduction of `set -e;` at the
   start of the command turned commands like `DO_BUILD=1 cmd` into
   `DO_BUILD=1 set -e; cmd` which means that `cmd` was running without
   an environment.

Simplify the code for computing the environment: remove the dead bits,
and use `export KEY=VAL;` to make sure all commands in the script get
the values set (since we often use `;` in our .execute() lines now).
Improve the quoting by using `shlex.quote()` instead of just adding
single quotes and hoping it will be OK.

machine/machine_core/ssh_connection.py
@@ -277,17 +277,6 @@ class SSHConnection(object):
         if not self.__ssh_direct_opt_var(direct=direct):
             self._ensure_ssh_master()
 
-        env_script = ""
-        env_command = []
-        if environment and isinstance(environment, dict):
-            for name, value in environment.items():
-                env_script += "%s='%s'\n" % (name, value)
-                env_script += "export %s\n" % name
-                env_command.append("{}={}".format(name, value))
-        elif environment == {}:
-            pass
-        else:
-            raise Exception("enviroment support dict or list items given: {0}".format(environment))
         default_ssh_params = [
             "ssh",
             "-p", str(self.ssh_port),
@@ -297,7 +286,9 @@ class SSHConnection(object):
             self.ssh_address
         ]
         additional_ssh_params = []
+
         cmd = ['set -e;']
+        cmd += [f'export {name}={shlex.quote(value)}; ' for name, value in environment.items()]
 
         additional_ssh_params += self.__execution_opts(direct=direct)
 
@@ -310,7 +301,7 @@ class SSHConnection(object):
             cmd.append(' '.join(shlex.quote(arg) for arg in command))
             if not quiet:
                 self.message("+", *command)
-        command_line = ssh_env + default_ssh_params + additional_ssh_params + env_command + cmd
+        command_line = ssh_env + default_ssh_params + additional_ssh_params + cmd
 
         with timeoutlib.Timeout(seconds=timeout, error_message="Timed out on '%s'" % command, machine=self):
             res = subprocess.run(command_line,