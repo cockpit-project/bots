cockpit-project/bots

commit e311d26c10bfbd7cb74a7643615bf5f4bb0d60ca

task: Report task status/logs directly to GitHub/S3, drop sink
We are using the whole sink machinery only for image-refresh logs.  The
sink infrastructure is somewhat precarious (infrastructure is going
away), and it's also overkill.

Change image-refresh to always upload the log to S3. In CI we always
want this, and developers locally don't use this bot but `image-create`
directly.

Rework the task begin/end reporting to change titles and push comments
to GitHub directly, instead of indirecting via the sink.

Introduce a new protocol for a tasks's run function return value: We now
need to communicate failure but also pass a log URL, so support
returning a (retcode, log_url) tuple. Use that in image-refresh.

This requires extending the mock GitHub API in test-task a bit, to cover
the calls that report_finish() now does.

task/__init__.py
@@ -31,7 +31,6 @@ import time
 import traceback
 
 from . import github
-from . import sink
 from lib.constants import BASE_DIR
 
 
@@ -90,8 +89,6 @@ def main(**kwargs):
     parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
     parser.add_argument("--issue", dest="issue", action="store",
                         help="Act on an already created task issue")
-    parser.add_argument("--publish", dest="publish", default=os.environ.get("TEST_PUBLISH", ""),
-                        action="store", help="Publish results centrally to a sink")
     parser.add_argument("--dry", dest="dry", action="store_true",
                         help="Dry run to validate this task if supported")
     parser.add_argument("context", nargs="?")
@@ -104,10 +101,12 @@ def main(**kwargs):
     if "verbose" not in task:
         task["verbose"] = opts.verbose
     task["issue"] = opts.issue
-    task["publish"] = opts.publish
     task["dry"] = opts.dry
 
     ret = run(opts.context, **task)
+    if isinstance(ret, tuple):
+        # some bots pass (exitcode, log_url)
+        ret = ret[0]
 
     if ret:
         sys.stderr.write("{0}: {1}\n".format(task["name"], ret))
@@ -122,132 +121,57 @@ def named(task):
         return os.path.basename(os.path.realpath(sys.argv[0]))
 
 
-def begin(publish, name, context, issue):
-    if not publish:
-        return None
-
-    hostname = socket.gethostname().split(".")[0]
-    current = time.strftime('%Y%m%d-%H%M%M')
-
-    # Update the body for an existing issue
-    if issue:
-        number = issue["number"]
-        identifier = "{0}-{1}-{2}".format(name, number, current)
-        title = issue["title"]
-        wip = "WIP: {0}: [no-test] {1}".format(hostname, title)
-        requests = [{
-            "method": "POST",
-            "resource": api.qualify("issues/{0}".format(number)),
-            "data": {"title": wip}
-        }, {
-            "method": "POST",
-            "resource": api.qualify("issues/{0}/comments".format(number)),
-            "data": {"body": "{0} in progress on {1}.\nLog: :link".format(name, hostname)}
-        }]
-        watches = [{
-            "resource": api.qualify("issues/{0}".format(number)),
-            "result": {"title": wip}
-        }]
-        aborted = [{
-            "method": "POST",
-            "resource": api.qualify("issues/{0}".format(number)),
-            "data": {"title": title}
-        }, {
-            "method": "POST",
-            "resource": api.qualify("issues/{0}/comments".format(number)),
-            "data": {"body": "Task aborted."}
-        }]
-    else:
-        identifier = "{0}-{1}".format(name, current)
-        requests = []
-        watches = []
-        aborted = []
-
-    status = {
-        "github": {
-            "token": api.token,
-            "requests": requests,
-            "watches": watches
-        },
-
-        "onaborted": {
-            "github": {
-                "token": api.token,
-                "requests": aborted
-            }
-        }
-    }
-
-    publishing = sink.Sink(publish, identifier, status)
-    sys.stderr.write("# Task: {0} {1}\n# Host: {2}\n\n".format(name, context or "", hostname))
-
-    # For statistics
-    publishing.start = time.time()
-
-    return publishing
-
-
-def finish(publishing, ret, name, context, issue):
-    if not publishing:
+def report_begin(name, context, issue, dry=False):
+    if dry:
         return
 
-    if not ret:
-        comment = None
-        result = "Completed"
-    elif isinstance(ret, str):
-        comment = "{0}: :link".format(ret)
-        result = ret
-    else:
-        comment = "Task failed: :link"
-        result = "Failed"
+    hostname = socket.gethostname().split(".")[0]
 
-    duration = int(time.time() - publishing.start)
-    sys.stderr.write("\n# Result: {0}\n# Duration: {1}s\n".format(result, duration))
-
+    # Update the requesting issue
     if issue:
-        # Note that we check whether pass or fail ... this is because
-        # the task is considered "done" until a human comes through and
-        # triggers it again by unchecking the box.
-        item = "{0} {1}".format(name, context or "").strip()
-        checklist = github.Checklist(issue["body"])
-        checklist.check(item, ret and "FAIL" or True)
-
         number = issue["number"]
+        title = issue["title"]
+        # set issue title to machine which processes the request
+        api.post(api.qualify(f"issues/{number}"), {"title": f"WIP: {hostname}: [no-test] {title}"})
 
-        # The sink wants us to escape colons :S
-        body = checklist.body.replace(':', '::')
 
-        requests = [{
-            "method": "POST",
-            "resource": api.qualify("issues/{0}".format(number)),
-            "data": {"title": "{0}".format(issue["title"]), "body": body}
-        }]
-
-        # Close the issue if it's not a pull request, successful, and all tasks done
-        if "pull_request" not in issue and not ret and len(checklist.items) == len(checklist.checked()):
-            requests[0]["data"]["state"] = "closed"
-
-        # Comment if there was a failure
-        if comment:
-            requests.insert(0, {
-                "method": "POST",
-                "resource": api.qualify("issues/{0}/comments".format(number)),
-                "data": {"body": comment}
-            })
-
+def report_finish(ret, name, context, issue, duration, dry=False):
+    if not ret:
+        result = "Completed"
+        log_comment = "Completed (no log available)"
+    elif isinstance(ret, tuple):  # (retcode, log_url)
+        result = "Success" if ret[0] == 0 else "Failed"
+        log_comment = f"{result}. Log: {ret[1]}"
+    elif isinstance(ret, str):
+        # log (unknown if success or failure)
+        result = "Completed"
+        log_comment = f"{result}. Log: {ret}"
     else:
-        requests = []
+        result = "Failed"
+        log_comment = "Task failed (no log available)"
 
-    publishing.status['github']['requests'] = requests
-    publishing.status['github']['watches'] = None
-    publishing.status['github']['onaborted'] = None
-    publishing.flush()
+    sys.stderr.write(f"\n# {log_comment}\n# Duration: {duration}s\n")
+
+    if not issue or dry:
+        return
+
+    # Note that we check whether pass or fail ... this is because
+    # the task is considered "done" until a human comes through and
+    # triggers it again by unchecking the box.
+    item = "{0} {1}".format(name, context or "").strip()
+    checklist = github.Checklist(issue["body"])
+    checklist.check(item, result == "Failed" and "FAIL" or True)
+
+    number = issue["number"]
+
+    api.post(api.qualify(f"issues/{number}"), {"title": issue["title"], "body": checklist.body})
+    comment(number, log_comment)
 
 
 def run(context, function, **kwargs):
     number = kwargs.get("issue", None)
-    publish = kwargs.get("publish", "")
     name = kwargs["name"]
+    dry = kwargs.get("dry", False)
 
     issue = None
     if number:
@@ -256,10 +180,13 @@ def run(context, function, **kwargs):
             return "No such issue: {0}".format(number)
         elif issue["title"].startswith("WIP:"):
             return "Issue is work in progress: {0}: {1}\n".format(number, issue["title"])
+        issue["number"] = number
         kwargs["issue"] = issue
         kwargs["title"] = issue["title"]
 
-    publishing = begin(publish, name, context, issue=issue)
+    start_time = time.time()
+
+    report_begin(name, context, issue=issue, dry=dry)
 
     ret = "Task threw an exception"
     try:
@@ -274,7 +201,7 @@ def run(context, function, **kwargs):
     except Exception:
         traceback.print_exc()
     finally:
-        finish(publishing, ret, name, context, issue)
+        report_finish(ret, name, context, issue, time.time() - start_time, dry=dry)
     return ret or 0
 
 

image-refresh
@@ -20,11 +20,14 @@
 import os
 import subprocess
 import sys
+import time
+import urllib.parse
 
 import task
 
 from lib.constants import BOTS_DIR, SCRIPTS_DIR
 from lib import testmap
+from lib import stores, s3
 
 
 sys.dont_write_bytecode = True
@@ -48,29 +51,40 @@ def run(image, verbose=False, **kwargs):
     # create the new image
     cmd = [os.path.join(BOTS_DIR, "image-create"), "--verbose", "--upload", image]
     os.environ['VIRT_BUILDER_NO_CACHE'] = "yes"
-    ret = subprocess.call(cmd)
-    if ret:
-        return ret
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    log = result.stdout
+    if result.returncode == 0:
+        # compare it to the previous one (on hosts we can ssh to)
+        if old_image and os.path.exists(os.path.join(SCRIPTS_DIR, image + '.setup')):
+            result = subprocess.run([os.path.join(BOTS_DIR, "image-diff"), old_image, image],
+                                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            log += result.stdout
 
-    # compare it to the previous one (on hosts we can ssh to)
-    if old_image and os.path.exists(os.path.join(SCRIPTS_DIR, image + '.setup')):
-        subprocess.check_call([os.path.join(BOTS_DIR, "image-diff"), old_image, image])
+    # upload the log to S3
+    log_url = os.path.join(
+        os.getenv('S3_LOGS_URL', stores.LOG_STORE),
+        f"image-refresh-logs/{image}-{time.strftime('%Y%m%d-%H%M%M')}.log"
+    )
+    with s3.urlopen(urllib.parse.urlparse(log_url), data=log, method="PUT",
+                    headers={"Content-Type": "text/plain", s3.ACL: s3.PUBLIC}):
+        pass
 
-    # create branch and push it
-    branch = task.branch(image, "images: Update {0} image".format(image), pathspec="images", **kwargs)
+    if result.returncode == 0:
+        # create branch and push it
+        branch = task.branch(image, "images: Update {0} image".format(image), pathspec="images", **kwargs)
 
-    # trigger tests if it is not a pull request
-    if branch and "pull" not in kwargs:
-        pull = task.pull(branch, labels=['bot', 'no-test'], run_tests=False, **kwargs)
+        # trigger tests if it is not a pull request
+        if branch and "pull" not in kwargs:
+            pull = task.pull(branch, labels=['bot', 'no-test'], run_tests=False, **kwargs)
 
-        # Trigger this pull request
-        api = task.github.GitHub()
-        head = pull["head"]["sha"]
-        for trigger in triggers:
-            api.post("statuses/{0}".format(head), {"state": "pending", "context": trigger,
-                                                   "description": task.github.NOT_TESTED_DIRECT})
+            # Trigger this pull request
+            api = task.github.GitHub()
+            head = pull["head"]["sha"]
+            for trigger in triggers:
+                api.post("statuses/{0}".format(head), {"state": "pending", "context": trigger,
+                                                       "description": task.github.NOT_TESTED_DIRECT})
 
-    return 0
+    return (result.returncode, log_url)
 
 
 if __name__ == '__main__':

lib/network.py
@@ -30,8 +30,6 @@ CA_PEM = os.getenv("COCKPIT_CA_PEM", os.path.join(IMAGES_DIR, "files", "ca.pem")
 
 CA_PEM_DOMAINS = [
     "e2e.bos.redhat.com",
-    # points to our AWS sink; obsolete after 2022-07 when we settled down on "S3 logs only"
-    "logs.cockpit-project.org",
     # development/cockpituous project tests
     "localdomain",
 ]

task/sink.py
@@ -1,80 +1,0 @@
-# This file is part of Cockpit.
-#
-# Copyright (C) 2015 Red Hat, Inc.
-#
-# Cockpit is free software; you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 2.1 of the License, or
-# (at your option) any later version.
-#
-# Cockpit is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
-
-import json
-import os
-import subprocess
-import sys
-
-__all__ = (
-    'Sink',
-)
-
-
-class Sink(object):
-    def __init__(self, host, identifier, status=None):
-        self.status = status
-
-        # Start a gzip and cat processes
-        self.ssh = subprocess.Popen(["ssh", "-o", "ServerAliveInterval=30", host, "--", "./sink", identifier],
-                                    stdin=subprocess.PIPE)
-
-        # Send the status line
-        self.ssh.stdin.write(json.dumps(status).encode('utf-8') + b"\n")
-        self.ssh.stdin.flush()
-
-        # Now dup our own output and errors into the pipeline
-        sys.stdout.flush()
-        self.fout = os.dup(1)
-        os.dup2(self.ssh.stdin.fileno(), 1)
-        sys.stderr.flush()
-        self.ferr = os.dup(2)
-        os.dup2(self.ssh.stdin.fileno(), 2)
-
-    def flush(self, status=None):
-        assert self.ssh is not None
-
-        # Reset stdout back
-        sys.stdout.flush()
-        os.dup2(self.fout, 1)
-        os.close(self.fout)
-        self.fout = -1
-
-        # Reset stderr back
-        sys.stderr.flush()
-        os.dup2(self.ferr, 2)
-        os.close(self.ferr)
-        self.ferr = -1
-
-        # Splice in the github status
-        if status is None:
-            status = self.status
-        if status is not None:
-            self.ssh.stdin.write(b"\n" + json.dumps(status).encode('utf-8'))
-
-        # All done sending output
-        try:
-            self.ssh.stdin.close()
-        except BrokenPipeError:
-            # SSH already terminated
-            pass
-
-        # SSH should terminate by itself; It legitimately exits with 77 on collisions
-        ret = self.ssh.wait()
-        if ret not in [0, 77]:
-            raise subprocess.CalledProcessError(ret, "ssh")
-        self.ssh = None

task/test-task
@@ -38,7 +38,8 @@ DATA = {
         "default_branch": "main"
     },
     "/repos/project/repo/issues/3333": {
-        "title": "The issue title"
+        "title": "The issue title",
+        "body": "Some bug\n - [ ] image-refresh foonux\n",
     },
     "/repos/project/repo/pulls/1234": {
         "title": "Task title",
@@ -99,6 +100,8 @@ def mockServer():
                 content_len = int(self.headers.get('content-length'))
                 data = json.loads(self.rfile.read(content_len).decode('utf-8'))
                 self.replyJson(data)
+            elif self.path.startswith("/repos/project/repo/issues/3333"):
+                self.replyJson({})
             else:
                 self.send_error(405, 'Method not allowed: ' + self.path)
 

commit 66b2dabae8c7348ce25e2c6c0bd1618a43505bb0

task: Fix issue.number to be an integer
This fixes a regression from commit e311d26c10 that fails to turn an
existing issue into a pull request.

task/__init__.py
@@ -180,7 +180,7 @@ def run(context, function, **kwargs):
             return "No such issue: {0}".format(number)
         elif issue["title"].startswith("WIP:"):
             return "Issue is work in progress: {0}: {1}\n".format(number, issue["title"])
-        issue["number"] = number
+        issue["number"] = int(number)
         kwargs["issue"] = issue
         kwargs["title"] = issue["title"]
 