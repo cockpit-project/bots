cockpit-project/bots

commit 3e741ec2a69efad86f5236ed12f9c261cbd1697c

s3: retry 503 errors on S3
According to the S3 documentation[1] we should retry if we get 503
errors.

The default configuration for various other S3 tools is 3 tries, so
let's do the same.

Fixes #3991

[1] https://aws.amazon.com/premiumsupport/knowledge-center/s3-resolve-503-slowdown-throttling/

lib/s3.py
@@ -111,16 +111,22 @@ def sign_curl(url: urllib.parse.ParseResult, method='GET', headers={}, checksum=
 
 def urlopen(url: urllib.parse.ParseResult, method='GET', headers={}, data=b'') -> IO:
     """Same as sign_request() but calls urlopen() on the result"""
-    headers = sign_request(url, method, headers, hashlib.sha256(data).hexdigest())
-    request = urllib.request.Request(url.geturl(), headers=headers, method=method, data=data)
-    try:
-        response = urllib.request.urlopen(request, context=host_ssl_context(url.netloc))
-    except urllib.error.HTTPError as exc:
-        logger.debug('%s %s %s → %s:', method, url.geturl(), headers, exc.status)
-        logger.debug('  %s', exc.read())
-        raise
-
-    return response
+    tries = 3
+    while True:
+        headers = sign_request(url, method, headers, hashlib.sha256(data).hexdigest())
+        request = urllib.request.Request(url.geturl(), headers=headers, method=method, data=data)
+        try:
+            return urllib.request.urlopen(request, context=host_ssl_context(url.netloc))
+        except urllib.error.HTTPError as exc:
+            logger.debug('%s %s %s → %s:', method, url.geturl(), headers, exc.status)
+            logger.debug('  %s', exc.read())
+            if exc.status == 503:
+                tries -= 1
+                if tries != 0:
+                    # First wait 1s, then wait 10s
+                    time.sleep(10 ** (2 - tries))
+                    continue
+            raise
 
 
 def list_bucket(url: urllib.parse.ParseResult) -> ET:

commit ef9d0b81eb15938623ce035bc780dffe17257445

s3: Retry 503 errors harder
Commit 3e741ec2a6 introduced retrying 503 errors. They still happen, so
retry a fourth time after 64 seconds.

Change the retry loop to count upwards, to avoid repeating the maximum
count in the exponential back-off calculation. Change the latter to be
base-4 to get more reasonable times for higher exponents.

lib/s3.py
@@ -111,20 +111,20 @@ def sign_curl(url: urllib.parse.ParseResult, method='GET', headers={}, checksum=
 
 def urlopen(url: urllib.parse.ParseResult, method='GET', headers={}, data=b'') -> IO:
     """Same as sign_request() but calls urlopen() on the result"""
-    tries = 3
+    retries = 0
     while True:
         headers = sign_request(url, method, headers, hashlib.sha256(data).hexdigest())
         request = urllib.request.Request(url.geturl(), headers=headers, method=method, data=data)
         try:
             return urllib.request.urlopen(request, context=host_ssl_context(url.netloc))
         except urllib.error.HTTPError as exc:
-            logger.debug('%s %s %s → %s:', method, url.geturl(), headers, exc.status)
+            logger.debug('%s %s %s attempt #%i → %s:', method, url.geturl(), headers, retries, exc.status)
             logger.debug('  %s', exc.read())
             if exc.status == 503:
-                tries -= 1
-                if tries != 0:
-                    # First wait 1s, then wait 10s
-                    time.sleep(10 ** (2 - tries))
+                if retries <= 3:
+                    # 1 → 4 → 16 → 64 s back-off
+                    time.sleep(4 ** retries)
+                    retries += 1
                     continue
             raise
 