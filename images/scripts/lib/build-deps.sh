6ab6ee367fd93337f2e5eb2365406beab5e309c9
Jiri Konecny <jkonecny@redhat.com>
2023-03-24 14:39:58 +0100

    Enable Anaconda build deps for other branches
    In the past only Fedora 37 image had Anaconda dependencies. Let's change
    that and add the dependencies to all the branches.
    
    This solution will try if Anaconda has a branch for given Fedora version
    in format "fedora-XX" and if that fails let's use Rawhide branch of
    Anaconda.
    This solution is required mainly because Fedora Rawhide is name
    fedora-XX in the script.

--- images/scripts/lib/build-deps.sh
+++ images/scripts/lib/build-deps.sh
@@ -25,12 +25,16 @@ esac
 
 echo "$spec" | rpmspec -D "$OS_VER" -D 'version 0' --buildrequires --query /dev/stdin | sed 's/.*/"&"/' | tr '\n' ' '
 
-# We build anaconda rpms on fedora-37
-if [ "$OS_VER" = 'fedora 37' ]; then
-    $GET https://raw.githubusercontent.com/rhinstaller/anaconda/master/anaconda.spec.in | \
-        sed 's/@PACKAGE.*@/0/' | rpmspec --buildrequires --query /dev/stdin | \
-        sed 's/.*/"&"/' | tr '\n' ' '
-fi
+# We build anaconda rpms
+case "$OS_VER" in
+    fedora*) # let's try if Anaconda have branch for the given fedora, if not, use master (rawhide)
+        SPEC=$($GET "https://raw.githubusercontent.com/rhinstaller/anaconda/fedora-${OS_VER#fedora }/anaconda.spec.in") || \
+            SPEC=$($GET "https://raw.githubusercontent.com/rhinstaller/anaconda/master/anaconda.spec.in")
+        echo "$SPEC" | \
+            sed 's/@PACKAGE.*@/0/' | rpmspec --buildrequires --query /dev/stdin | \
+            sed 's/.*/"&"/' | tr '\n' ' '
+    ;;
+esac
 
 # some extra build dependencies:
 # - libappstream-glib for validating appstream metadata in starter-kit and derivatives
