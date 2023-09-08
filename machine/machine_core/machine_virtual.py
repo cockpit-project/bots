cockpit-project/bots

commit d60055284cb81ac9e2cf0e5d3e3e4b25d7cbfc30

Print VM's console log when boot or poweroff times out

machine/machine_core/machine_virtual.py
@@ -86,8 +86,9 @@ TEST_DOMAIN_XML = """
     <graphics type='vnc' autoport='yes' listen='127.0.0.1'>
       <listen type='address' address='127.0.0.1'/>
     </graphics>
-    <console type='pty'>
+    <console type='{console_type}'>
       <target type='serial' port='0'/>
+      {console_source}
     </console>
     <disk type='file' device='cdrom'>
       <source file='{iso}'/>
@@ -245,12 +246,14 @@ class VirtMachine(Machine):
     cpus = None
 
     def __init__(self, image, networking=None, maintain=False, memory_mb=None, cpus=None,
-                 graphics=False, **args):
+                 capture_console=False, graphics=False, **args):
         self.maintain = maintain
 
         self.memory_mb = memory_mb or VirtMachine.memory_mb or MEMORY_MB
         self.cpus = cpus or VirtMachine.cpus or 1
         self.graphics = graphics
+        if capture_console:
+            self.console_file = tempfile.NamedTemporaryFile(suffix='.log', prefix='console-')
 
         # Set up some temporary networking info if necessary
         if networking is None:
@@ -331,6 +334,8 @@ class VirtMachine(Machine):
             "memory_in_mib": self.memory_mb,
             "drive": image_to_use,
             "iso": os.path.join(BOTS_DIR, "machine", "cloud-init.iso"),
+            "console_type": "file" if self.console_file else "pty",
+            "console_source": f"<source path='{self.console_file.name}'/>" if self.console_file else "",
         }
 
         if os.path.exists("/dev/kvm"):
@@ -472,6 +477,7 @@ class VirtMachine(Machine):
                     raise
                 time.sleep(1)
             else:
+                self.print_console_log()
                 raise Failure("Waiting for machine poweroff timed out")
             try:
                 with stdchannel_redirected(sys.stderr, os.devnull):
@@ -573,3 +579,23 @@ class VirtMachine(Machine):
         # On atomic systems, we need a hack to change files in /usr/lib/systemd
         if self.ostree_image:
             self.execute("mount -o remount,rw /usr")
+
+    def print_console_log(self):
+        """Prints VM's console to stderr"""
+        file_name = self.console_file.name
+
+        if not file_name:
+            return
+
+        try:
+            with open(file_name) as f:
+                log = f.read().strip()
+        except OSError as ex:
+            sys.stderr.write(f"Failed to open '{file_name}': {ex}\n")
+            return
+
+        if not log:
+            sys.stderr.write(f"VM's console log file '{file_name}' is empty\n")
+            return
+
+        sys.stderr.write(f"---- Console log starts here ----\n{log}\n---- Console log ends here ----\n")

image-create
@@ -38,6 +38,7 @@ parser.add_argument('-v', '--verbose', action='store_true', help='Display verbos
 parser.add_argument('-s', '--sit', action='store_true', help='Sit and wait if setup script fails')
 parser.add_argument('-n', '--no-save', action='store_true', help='Don\'t save the new image')
 parser.add_argument('-u', '--upload', action='store_true', help='Upload the image after creation')
+parser.add_argument('--capture-console', action='store_true', help='Capture VM console to stderr')
 parser.add_argument('--no-build', action='store_true', dest='no_build',
                     help='Don''t build packages and create the vm without build capabilities')
 parser.add_argument("--store", default=None, help="Where to send images")
@@ -207,7 +208,12 @@ try:
         memory_mb = 3072
     else:
         memory_mb = 2048
-    machine = testvm.VirtMachine(verbose=args.verbose, image=args.image, memory_mb=memory_mb, maintain=True)
+
+    machine = testvm.VirtMachine(verbose=args.verbose,
+                                 capture_console=args.capture_console,
+                                 image=args.image,
+                                 memory_mb=memory_mb,
+                                 maintain=True)
     builder = MachineBuilder(machine)
     builder.build()
     if not args.no_save:

image-refresh
@@ -46,7 +46,7 @@ def run(image, verbose=False, **kwargs):
         old_image = None
 
     # create the new image
-    cmd = [os.path.join(BOTS_DIR, "image-create"), "--verbose", "--upload", image]
+    cmd = [os.path.join(BOTS_DIR, "image-create"), "--verbose", "--capture-console", "--upload", image]
     os.environ['VIRT_BUILDER_NO_CACHE'] = "yes"
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     log = result.stdout

machine/machine_core/machine.py
@@ -71,7 +71,12 @@ class Machine(ssh_connection.SSHConnection):
         if not label and image != "unknown":
             label = "{}-{}-{}".format(image, ssh_address, ssh_port)
 
-        super(Machine, self).__init__(user, ssh_address, ssh_port, identity_file, verbose=verbose, label=label)
+        super(Machine, self).__init__(user,
+                                      ssh_address,
+                                      ssh_port,
+                                      identity_file,
+                                      verbose=verbose,
+                                      label=label)
 
         self.arch = arch
         self.image = image

machine/machine_core/ssh_connection.py
@@ -119,6 +119,9 @@ class SSHConnection(object):
             time.sleep(1)
         raise exceptions.Failure("Timed out waiting for /run/nologin to disappear")
 
+    def print_console_log(self):
+        pass
+
     def wait_boot(self, timeout_sec=120):
         """Wait for a machine to boot"""
         start_time = time.time()
@@ -129,6 +132,7 @@ class SSHConnection(object):
                 if boot_id:
                     break
         if not boot_id:
+            self.print_console_log()
             raise exceptions.Failure("Unable to reach machine {0} via ssh: {1}:{2}".format(
                 self.label, self.ssh_address, self.ssh_port))
         self.boot_id = boot_id

commit f7c20b47d5af6db57f73cacb758f5e4454fdefd7

machine: Fix crash when not capturing console
Regression introduced in commit d600552.

machine/machine_core/machine_virtual.py
@@ -254,6 +254,8 @@ class VirtMachine(Machine):
         self.graphics = graphics
         if capture_console:
             self.console_file = tempfile.NamedTemporaryFile(suffix='.log', prefix='console-')
+        else:
+            self.console_file = None
 
         # Set up some temporary networking info if necessary
         if networking is None:
@@ -582,10 +584,10 @@ class VirtMachine(Machine):
 
     def print_console_log(self):
         """Prints VM's console to stderr"""
-        file_name = self.console_file.name
-
-        if not file_name:
+        if not self.console_file:
             return
+
+        file_name = self.console_file.name
 
         try:
             with open(file_name) as f: