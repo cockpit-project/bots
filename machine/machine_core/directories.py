4c553273920f0d5025f9f2d2a795242db677dffe
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2024-04-21 10:12:20 +0200

    machine: drop a pair of deprecated files
    "This will go away soon.", says 2021.

--- machine/machine_core/directories.py
+++
@@ -1,9 +0,0 @@
-# Backwards compatibility shim.  This will go away soon.
-
-from lib.directories import get_images_data_dir
-
-__all__ = ("get_images_data_dir",)
-
-import warnings
-
-warnings.warn('machine_core.directories is deprecated.  Use lib.directories', DeprecationWarning)
