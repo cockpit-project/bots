9acb9c9d56791e0adecd653cbd33f70708bd2b8c
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2024-04-23 18:25:07 +0200

    test: port test_cache to pytest
    ...and add strict types.

--- test/test_cache.py
+++ test/test_cache.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # This file is part of Cockpit.
 #
 # Copyright (C) 2017 Red Hat, Inc.
@@ -17,74 +15,62 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
 
-import os
-import tempfile
 import time
-import unittest
+from pathlib import Path
 
 from task import cache
 
 
-class TestCache(unittest.TestCase):
-    def setUp(self):
-        self.directory = tempfile.mkdtemp()
+def test_read_write(tmp_path: Path) -> None:
+    value = {"blah": 1}
 
-    def tearDown(self):
-        for name in os.listdir(self.directory):
-            os.unlink(os.path.join(self.directory, name))
-        os.rmdir(self.directory)
+    c = cache.Cache[object](f'{tmp_path}')
+    result = c.read(r"pa+t\%h")
+    assert result is None
 
-    def testReadWrite(self):
-        value = {"blah": 1}
+    c.write(r"pa+t\%h", value)
+    result = c.read(r"pa+t\%h")
+    assert result == value
 
-        c = cache.Cache(self.directory)
-        result = c.read(r"pa+t\%h")
-        self.assertIsNone(result)
+    other = "other"
+    c.write(r"pa+t\%h", other)
+    result = c.read(r"pa+t\%h")
+    assert result == other
 
-        c.write(r"pa+t\%h", value)
-        result = c.read(r"pa+t\%h")
-        self.assertEqual(result, value)
-
-        other = "other"
-        c.write(r"pa+t\%h", other)
-        result = c.read(r"pa+t\%h")
-        self.assertEqual(result, other)
-
-        c.write("second", value)
-        result = c.read(r"pa+t\%h")
-        self.assertEqual(result, other)
-
-    def testCurrent(self):
-        c = cache.Cache(self.directory, lag=3)
-
-        c.write("resource2", {"value": 2})
-        self.assertTrue(c.current("resource2"))
-
-        time.sleep(2)
-        self.assertTrue(c.current("resource2"))
-
-        time.sleep(2)
-        self.assertFalse(c.current("resource2"))
-
-    def testCurrentMark(self):
-        c = cache.Cache(self.directory, lag=3)
-
-        self.assertFalse(c.current("resource"))
-
-        c.write("resource", {"value": 1})
-        self.assertTrue(c.current("resource"))
-
-        time.sleep(2)
-        self.assertTrue(c.current("resource"))
-
-        c.mark()
-        self.assertFalse(c.current("resource"))
-
-    def testCurrentZero(self):
-        c = cache.Cache(self.directory, lag=0)
-        c.write("resource", {"value": 1})
-        self.assertFalse(c.current("resource"))
+    c.write("second", value)
+    result = c.read(r"pa+t\%h")
+    assert result == other
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_current(tmp_path: Path) -> None:
+    c = cache.Cache[object](f'{tmp_path}', lag=3)
+
+    c.write("resource2", {"value": 2})
+    assert c.current('resource2') is True
+
+    time.sleep(2)
+    assert c.current('resource2') is True
+
+    time.sleep(2)
+    assert c.current('resource2') is False
+
+
+def test_current_mark(tmp_path: Path) -> None:
+    c = cache.Cache[object](f'{tmp_path}', lag=3)
+
+    assert c.current('resource') is False
+
+    c.write("resource", {"value": 1})
+    assert c.current('resource') is True
+
+    time.sleep(2)
+    assert c.current('resource') is True
+
+    c.mark()
+    assert c.current('resource') is False
+
+
+def test_current_zero(tmp_path: Path) -> None:
+    c = cache.Cache[object](f'{tmp_path}', lag=0)
+    c.write("resource", {"value": 1})
+    assert c.current('resource') is False
