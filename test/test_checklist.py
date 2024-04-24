e27d92baa455ccc802e51c3a8fb328f849878a1f
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2024-04-24 08:58:56 +0200

    test: port test_checklist.py to pytest
    ...and add types.

--- test/test_checklist.py
+++ test/test_checklist.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # This file is part of Cockpit.
 #
 # Copyright (C) 2017 Red Hat, Inc.
@@ -17,78 +15,77 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
 
-import unittest
-
 from task import github
 
 
-class TestChecklist(unittest.TestCase):
-    def testParse(self):
-        parse_line = github.Checklist.parse_line
-        self.assertEqual(parse_line("blah"), (None, None))
-        self.assertEqual(parse_line(""), (None, None))
-        self.assertEqual(parse_line(""), (None, None))
-        self.assertEqual(parse_line("* [ ] test two"), ("test two", False))
-        self.assertEqual(parse_line("- [ ] test two"), ("test two", False))
-        self.assertEqual(parse_line(" * [ ]  test two "), ("test two", False))
-        self.assertEqual(parse_line(" - [ ]  test two "), ("test two", False))
-        self.assertEqual(parse_line(" - [x] test two "), ("test two", True))
-        self.assertEqual(parse_line(" * [x] test two"), ("test two", True))
-        self.assertEqual(parse_line(" * [x] FAIL: test two"), ("test two", "FAIL"))
-        self.assertEqual(parse_line(" * [x] FAIL: test FAIL: two"), ("test FAIL: two", "FAIL"))
-        self.assertEqual(parse_line(" * [x]test three"), (None, None))
-        self.assertEqual(parse_line(" - [X] test two "), ("test two", True))
-        self.assertEqual(parse_line(" * [X] test four"), ("test four", True))
-        self.assertEqual(parse_line(" * [X] FAIL: test four"), ("test four", "FAIL"))
-        self.assertEqual(parse_line(" * [X] FAIL: test FAIL: four"), ("test FAIL: four", "FAIL"))
-        self.assertEqual(parse_line(" * [X]test five"), (None, None))
-
-    def testFormat(self):
-        format_line = github.Checklist.format_line
-        self.assertEqual(format_line("blah", True), " * [x] blah")
-        self.assertEqual(format_line("blah", False), " * [ ] blah")
-        self.assertEqual(format_line("blah", "FAIL"), " * [ ] FAIL: blah")
-
-    def testProcess(self):
-        body = "This is a description\n- [ ] item1\n * [x] Item two\n * [X] Item three\n\nMore lines"
-        checklist = github.Checklist(body)
-        self.assertEqual(checklist.body, body)
-        self.assertEqual(checklist.items, {"item1": False, "Item two": True, "Item three": True})
-
-    def testCheck(self):
-        body = "This is a description\n- [ ] item1\n * [x] Item two\n * [X] Item three\n\nMore lines"
-        checklist = github.Checklist(body)
-        checklist.check("item1", True)
-        checklist.check("Item three", False)
-        self.assertEqual(checklist.body,
-                         "This is a description\n * [x] item1\n * [x] Item two\n * [ ] Item three\n\nMore lines")
-        self.assertEqual(checklist.items, {"item1": True, "Item two": True, "Item three": False})
-
-    def testDisable(self):
-        body = "This is a description\n- [ ] item1\n * [x] Item two\n\nMore lines"
-        checklist = github.Checklist(body)
-        checklist.check("item1", "Status")
-        self.assertEqual(checklist.body,
-                         "This is a description\n * [ ] Status: item1\n * [x] Item two\n\nMore lines")
-        self.assertEqual(checklist.items, {"item1": "Status", "Item two": True})
-
-    def testAdd(self):
-        body = "This is a description\n- [ ] item1\n * [x] Item two\n\nMore lines"
-        checklist = github.Checklist(body)
-        checklist.add("Item three")
-        self.assertEqual(checklist.body,
-                         "This is a description\n- [ ] item1\n * [x] Item two\n\nMore lines\n * [ ] Item three")
-        self.assertEqual(checklist.items, {"item1": False, "Item two": True, "Item three": False})
-
-    def testChecked(self):
-        body = "This is a description\n- [ ] item1\n * [x] Item two\n * [X] Item three\n\nMore lines"
-        checklist = github.Checklist(body)
-        checklist.check("item1", True)
-        checklist.check("Item three", False)
-        checked = checklist.checked()
-        self.assertEqual(checklist.items, {"item1": True, "Item two": True, "Item three": False})
-        self.assertEqual(checked, {"item1": True, "Item two": True})
+def test_parse() -> None:
+    parse_line = github.Checklist.parse_line
+    assert parse_line("blah") == (None, None)
+    assert parse_line("") == (None, None)
+    assert parse_line("") == (None, None)
+    assert parse_line("* [ ] test two") == ("test two", False)
+    assert parse_line("- [ ] test two") == ("test two", False)
+    assert parse_line(" * [ ]  test two ") == ("test two", False)
+    assert parse_line(" - [ ]  test two ") == ("test two", False)
+    assert parse_line(" - [x] test two ") == ("test two", True)
+    assert parse_line(" * [x] test two") == ("test two", True)
+    assert parse_line(" * [x] FAIL: test two") == ("test two", "FAIL")
+    assert parse_line(" * [x] FAIL: test FAIL: two") == ("test FAIL: two", "FAIL")
+    assert parse_line(" * [x]test three") == (None, None)
+    assert parse_line(" - [X] test two ") == ("test two", True)
+    assert parse_line(" * [X] test four") == ("test four", True)
+    assert parse_line(" * [X] FAIL: test four") == ("test four", "FAIL")
+    assert parse_line(" * [X] FAIL: test FAIL: four") == ("test FAIL: four", "FAIL")
+    assert parse_line(" * [X]test five") == (None, None)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_format() -> None:
+    format_line = github.Checklist.format_line
+    assert format_line("blah", True) == " * [x] blah"
+    assert format_line("blah", False) == " * [ ] blah"
+    assert format_line("blah", "FAIL") == " * [ ] FAIL: blah"
+
+
+def test_process() -> None:
+    body = "This is a description\n- [ ] item1\n * [x] Item two\n * [X] Item three\n\nMore lines"
+    checklist = github.Checklist(body)
+    assert checklist.body == body
+    assert checklist.items == {"item1": False, "Item two": True, "Item three": True}
+
+
+def test_check() -> None:
+    body = "This is a description\n- [ ] item1\n * [x] Item two\n * [X] Item three\n\nMore lines"
+    checklist = github.Checklist(body)
+    checklist.check("item1", True)
+    checklist.check("Item three", False)
+    assert checklist.body == \
+                     "This is a description\n * [x] item1\n * [x] Item two\n * [ ] Item three\n\nMore lines"
+    assert checklist.items == {"item1": True, "Item two": True, "Item three": False}
+
+
+def test_disable() -> None:
+    body = "This is a description\n- [ ] item1\n * [x] Item two\n\nMore lines"
+    checklist = github.Checklist(body)
+    checklist.check("item1", "Status")
+    assert checklist.body == \
+                     "This is a description\n * [ ] Status: item1\n * [x] Item two\n\nMore lines"
+    assert checklist.items == {"item1": "Status", "Item two": True}
+
+
+def test_add() -> None:
+    body = "This is a description\n- [ ] item1\n * [x] Item two\n\nMore lines"
+    checklist = github.Checklist(body)
+    checklist.add("Item three")
+    assert checklist.body == \
+                     "This is a description\n- [ ] item1\n * [x] Item two\n\nMore lines\n * [ ] Item three"
+    assert checklist.items == {"item1": False, "Item two": True, "Item three": False}
+
+
+def test_checked() -> None:
+    body = "This is a description\n- [ ] item1\n * [x] Item two\n * [X] Item three\n\nMore lines"
+    checklist = github.Checklist(body)
+    checklist.check("item1", True)
+    checklist.check("Item three", False)
+    checked = checklist.checked()
+    assert checklist.items == {"item1": True, "Item two": True, "Item three": False}
+    assert checked == {"item1": True, "Item two": True}
