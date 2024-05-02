cockpit-project/bots

commit 3b03646ad5086b2ac3592ad8c2bb31c9cd4f74f6

task: simplify and type Checklist implementation
The printing/parsing in particular are easier to understand now.

task/__init__.py
@@ -30,7 +30,7 @@ import subprocess
 import sys
 import time
 import traceback
-from collections.abc import Sequence
+from collections.abc import Mapping, Sequence
 from datetime import datetime, timezone
 
 from lib.constants import BASE_DIR
@@ -221,7 +221,7 @@ def issue(
     state: str = "open",
     since: int | None = None,
     dry: bool = False
-) -> dict[str, object]:
+) -> Mapping[str, object]:
     if context:
         item = f"{item} {context}".strip()
 

task/github.py
@@ -28,7 +28,7 @@ import subprocess
 import time
 import urllib.parse
 import urllib.request
-from collections.abc import Sequence
+from collections.abc import Mapping, Sequence
 from http import HTTPStatus
 from ssl import SSLEOFError
 from typing import Any
@@ -410,38 +410,29 @@ class GitHub:
 
 
 class Checklist:
-    def __init__(self, body=None):
-        self.process(body or "")
+    def __init__(self, body: str = ''):
+        self.process(body)
 
     @staticmethod
-    def format_line(item, check):
-        status = ""
-        if isinstance(check, str):
-            status = check + ": "
-            check = False
-        return f" * [{'x' if check else ' '}] {status}{item}"
+    def format_line(item: str, check: bool | str) -> str:
+        if isinstance(check, bool):
+            return f' * [{" x"[check]}] {item}'  # ' * [ ] item' or ' * [x] item'
+        else:
+            return f' * [ ] {check}: {item}'  # eg ' * [ ] FAIL: item'
 
     @staticmethod
-    def parse_line(line):
-        check = item = None
-        stripped = line.strip()
-        if stripped[:6] in ["* [ ] ", "- [ ] ", "* [x] ", "- [x] ", "* [X] ", "- [X] "]:
-            status, _, item = stripped[6:].strip().partition(": ")
-            if not item:
-                item = status
-                status = None
-            if status:
-                check = status
-            else:
-                check = stripped[3] in ["x", "X"]
-        return (item, check)
+    def parse_line(line: str) -> tuple[str | None, str | bool | None]:
+        match = re.fullmatch(r'[*-] \[(?P<checked>[ xX])\]\s+((?P<status>[A-Z]+):\s+)?(?P<item>.+)', line.strip())
+        if match is None:
+            return None, None
+        return match['item'], match['status'] or match['checked'] in 'xX'
 
-    def process(self, body, items=None):
+    def process(self, body: str, items: Mapping[str, str | bool] = {}) -> None:
         self.items = {}
         lines = []
-        items = dict(items or {})
+        items = dict(items)
         for line in body.splitlines():
-            (item, check) = self.parse_line(line)
+            item, check = self.parse_line(line)
             if item:
                 if item in items:
                     check = items[item]
@@ -454,13 +445,13 @@ class Checklist:
             self.items[item] = check
         self.body = "\n".join(lines)
 
-    def check(self, item, checked=True):
+    def check(self, item: str, checked: str | bool = True) -> None:
         self.process(self.body, {item: checked})
 
-    def add(self, item):
+    def add(self, item: str) -> None:
         self.process(self.body, {item: False})
 
-    def checked(self):
+    def checked(self) -> Mapping[str, str | bool]:
         result = {}
         for item, check in self.items.items():
             if check:

commit 54d5984524b21f8b2796e7001e2b23d74319e4c4

task: fix empty body handling in Checklist()
This got broken by 3b03646ad508.

GitHub sends `body: null` for issues without bodies.

Add a testcase.

Fixes #6333

issue-scan
@@ -106,7 +106,7 @@ def tasks_for_issues(issues_data: str, opts_repo: str | None) -> Sequence[tuple[
         # the bots can do, then the bots will ignore this issue
         # (below in output_task)
         #
-        checklist = github.Checklist(get_str(issue, "body"))
+        checklist = github.Checklist(get_str(issue, "body", None))
         for item, checked in checklist.items.items():
             if not checked:
                 results.append((item, issue, api.repo))

task/__init__.py
@@ -231,7 +231,7 @@ def issue(
         since += random.randint(-3600, 3600)
 
     for issue in api.issues(state=state, since=since):
-        checklist = github.Checklist(get_str(issue, "body"))
+        checklist = github.Checklist(get_str(issue, "body", None))
         if item in checklist.items:
             return issue
 

task/github.py
@@ -442,8 +442,9 @@ class GitHub:
 
 
 class Checklist:
-    def __init__(self, body: str = ''):
-        self.process(body)
+    # NB: GitHub sends `body: null` for issues with empty bodies
+    def __init__(self, body: str | None):
+        self.process(body or '')
 
     @staticmethod
     def format_line(item: str, check: bool | str) -> str:

test/test_checklist.py
@@ -39,6 +39,11 @@ def test_parse() -> None:
     assert parse_line(" * [X]test five") == (None, None)
 
 
+def test_empty_body() -> None:
+    assert github.Checklist('').items == {}
+    assert github.Checklist(None).items == {}
+
+
 def test_format() -> None:
     format_line = github.Checklist.format_line
     assert format_line("blah", True) == " * [x] blah"