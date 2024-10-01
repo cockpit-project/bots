01f61d0b5d6016d6d24fa25dc3c6bcd420af596e
Martin Pitt <martin@piware.de>
2024-10-01 07:03:56 +0200

    images: Drop explicit libssh from Fedora
    The explicit runtime dependency has been unnecessary since commit
    9a6962d3c, as installing cockpit-bridge brings this in as a dependency.
    It will actually drop from the image after the next Cockpit release, as
    it was dropped in https://github.com/cockpit-project/cockpit/pull/19441
    
    Also drop it from the base container.

--- images/scripts/lib/base/setup.sh
+++ images/scripts/lib/base/setup.sh
@@ -2,5 +2,5 @@
 set -eu
 
 dnf -y update
-dnf install -y sed findutils glib-networking json-glib libssh openssl python3 systemd
+dnf install -y sed findutils glib-networking json-glib openssl python3 systemd
 dnf clean all
