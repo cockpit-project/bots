cockpit-project/bots

commit 0283f67188241dbc900f87e5adfad989d8604e9a

Use raw strings for `re` operations
Ruff 0.8 complains about that, like

> RUF039 [*] First argument to `re.search()` is not raw string

task/github.py
@@ -126,7 +126,7 @@ def get_origin_repo() -> str | None:
     except subprocess.CalledProcessError:
         return None
     url = res.decode('utf-8').strip()
-    m = re.fullmatch("(git@github.com:|https://github.com/)(.*?)(\\.git)?", url)
+    m = re.fullmatch(r"(git@github.com:|https://github.com/)(.*?)(\\.git)?", url)
     if m:
         return m.group(2).rstrip("/")
     raise RuntimeError("Not a GitHub repo: %s" % url)

image-upload
@@ -47,7 +47,7 @@ def upload(store: str, source: str, public: bool, prune: bool = False) -> bool:
     headers = {s3.ACL: s3.PUBLIC} if public else {}
 
     # slightly magic: we already know the checksum: it's part of the filename
-    match = re.search('[0-9a-f]{64}', source)
+    match = re.search(r'[0-9a-f]{64}', source)
     assert match is not None
     hash_value = match.group(0)
 

machine/machine_core/machine.py
@@ -162,7 +162,7 @@ class Machine(ssh_connection.SSHConnection):
         self.execute("sleep 1; journalctl --sync")
 
         # Prepend "SYSLOG_IDENTIFIER=" as a default field, for backwards compatibility
-        filters = (m if re.match("[a-zA-Z0-9_]+=", m) else "SYSLOG_IDENTIFIER=" + m for m in matches)
+        filters = (m if re.match(r"[a-zA-Z0-9_]+=", m) else "SYSLOG_IDENTIFIER=" + m for m in matches)
 
         # Some versions of journalctl terminate unsuccessfully when
         # the output is empty.  We work around this by ignoring the

task/__init__.py
@@ -292,7 +292,7 @@ def branch(context, message, pathspec=".", issue=None, push=True, branch=None, d
 
     if not branch:
         branch = f'{name}-{context or ""}-{datetime.now(tz=timezone.utc):%Y%m%d-%H%M%S}'
-        branch = re.sub('[^A-Za-z0-9]+', '-', branch)
+        branch = re.sub(r'[^A-Za-z0-9]+', '-', branch)
         execute("git", "checkout", "-b", branch)
 
     # Tell git about our github token for authentication

test-failure-policy
@@ -200,7 +200,7 @@ def update_known_issue(api: github.GitHub, number: int, err: str, context: str) 
     # try to find an existing comment to update; extract the traceback from the
     # whole output; also ensure to remove the "# duration: XXs" trailer
     err_key = normalize_traceback(err).strip()
-    m = re.search("^(Traceback.*^not ok[^#\\n]*)", err_key, re.S | re.M)
+    m = re.search(r"^(Traceback.*^not ok[^#\\n]*)", err_key, re.S | re.M)
     if m:
         err_key = m.group(1)
     comment_key = f"{context}\n"

test/test_aio.py
@@ -129,8 +129,8 @@ def test_lru_cache() -> None:
 def service() -> Iterator[GitHubService]:
     server = GitHubService()
     with aioresponses() as mock:
-        mock.post(re.compile(''), callback=server.post, repeat=True)
-        mock.get(re.compile(''), callback=server.get, repeat=True)
+        mock.post(re.compile(r''), callback=server.post, repeat=True)
+        mock.get(re.compile(r''), callback=server.get, repeat=True)
         yield server
 
 

commit 60afc4fa87527ebe1edb438b78359906d10f9f58

task: Fix get_origin_repo() .git extension matching
This got broken in commit 0283f67188241d, I forgot to unescape the
backslash.

task/github.py
@@ -126,7 +126,7 @@ def get_origin_repo() -> str | None:
     except subprocess.CalledProcessError:
         return None
     url = res.decode('utf-8').strip()
-    m = re.fullmatch(r"(git@github.com:|https://github.com/)(.*?)(\\.git)?", url)
+    m = re.fullmatch(r"(git@github.com:|https://github.com/)(.*?)(\.git)?", url)
     if m:
         return m.group(2).rstrip("/")
     raise RuntimeError("Not a GitHub repo: %s" % url)