cockpit-project/bots

commit 53f047eadad7182ad29aa31b62d778a93cd60aa8

stores: designate one of our S3 buckets "private"
Our attempts to mitigate scraper bots by restricting all access to
images is causing significant grief for new contributors: having to
privately message a few designated members of the Cockpit team in order
to get an S3 token to be able to start testing your first contribution
isn't particularly friendly or scalable.

One solution would be to generate a well-known "public" secret key for
downloading images.  The idea is that the scraper bots would not be
smart enough to use this key, but casual contributors would still be
able to come across it easily enough.  One issue with this approach is
that Linode's S3 ACLs are not sufficiently powerful to allow us to
specify that this key can only download the public (read: not RHEL)
images: we can only control access at the bucket level.

As a workaround, we can take advantage of the fact that we currently
have two more-or-less equal S3 buckets and arbitrarily designate the EU
one as "private" and the US one as "public".  We can upload the RHEL
images to only the "private" EU mirror and then give "public" access to
the US mirror for all the other images.

What happens with the new "public" secret key is currently up for
debate.  For the time being, let's keep it as a well-known secret passed
around Cockpit developers and shared freely with anyone who asks.  At
some point we may also consider to publish it in a README or even
integrate it into the downloader script directly.

image-download
@@ -47,7 +47,7 @@ from lib import s3
 from lib.constants import IMAGES_DIR
 from lib.directories import get_images_data_dir
 from lib.network import get_curl_ca_arg, redhat_network
-from lib.stores import PUBLIC_STORES, REDHAT_STORES
+from lib.stores import PRIVATE_STORES, PUBLIC_STORES, REDHAT_STORES
 from lib.testmap import get_test_image
 
 EPOCH = "Thu, 1 Jan 1970 00:00:00 GMT"
@@ -133,7 +133,11 @@ def download(dest: str, force: bool, state: bool, quiet: bool, stores: Sequence[
     name = os.path.basename(dest)
 
     if not stores:
-        stores = list(PUBLIC_STORES)
+        # Due to AI scraper-bot mitigation all public stores currently require
+        # an access token: if we don't have one, then don't bother.
+        stores = [
+            store for store in (*PUBLIC_STORES, *PRIVATE_STORES) if s3.is_key_present(urllib.parse.urlparse(store))
+        ]
         if redhat_network():
             stores += REDHAT_STORES
         image_upload_store = os.environ.get('COCKPIT_IMAGE_UPLOAD_STORE')
@@ -142,6 +146,11 @@ def download(dest: str, force: bool, state: bool, quiet: bool, stores: Sequence[
             # testing.  Compare with image-upload which *only* uploads to this
             # store, if it's present.
             stores += [image_upload_store]
+
+    # Could be that we have no stores due to missing tokens.  Print a helpful message for new contributors.
+    if not stores:
+        sys.exit("Due to excessive bandwidth usage caused by AI scrapers, it's not currently possible to download "
+                 "testing images without a token.  Please ask in the Cockpit Matrix channel if you need a token.")
 
     # The time condition for If-Modified-Since
     exists = not force and os.path.exists(dest)

image-upload
@@ -29,7 +29,7 @@ from lib import s3
 from lib.constants import BOTS_DIR, IMAGES_DIR
 from lib.directories import get_images_data_dir
 from lib.network import get_curl_ca_arg, redhat_network
-from lib.stores import PUBLIC_STORES, REDHAT_STORES
+from lib.stores import PRIVATE_STORES, PUBLIC_STORES, REDHAT_STORES
 
 
 def upload(store: str, source: str, public: bool, prune: bool = False) -> bool:
@@ -95,10 +95,7 @@ def main() -> None:
         sources.append(source)
 
     for source in sources:
-        # Temporarily make all images private, until we figure out if it's images or logs which cause
-        # so much traffic these days (AI scrapers?)
-        # public = not os.path.basename(source).startswith('rhel')
-        public = False
+        public = not os.path.basename(source).startswith('rhel')
 
         stores = args.store
         if not stores:
@@ -108,7 +105,12 @@ def main() -> None:
                 # adds this to the list, if it's present.
                 stores = [image_upload_store]
         if not stores:
-            stores = list(PUBLIC_STORES)
+            stores = list(PRIVATE_STORES)
+            if public:
+                # NB: We currently have a well-known S3 token which is capable
+                # of accessing all data uploaded to these buckets, regardless
+                # of ACL.  Only upload public images here.
+                stores += PUBLIC_STORES
             if redhat_network():
                 stores += REDHAT_STORES
         success = False

lib/stores.py
@@ -21,8 +21,12 @@ from lib.directories import xdg_config_home
 
 # hosted on public internet
 PUBLIC_STORES: Sequence[str] = (
-    "https://cockpit-images.eu-central-1.linodeobjects.com/",
     "https://cockpit-images.us-east-1.linodeobjects.com/",
+)
+
+# hosted on the public internet, only accessible with a private token
+PRIVATE_STORES: Sequence[str] = (
+    "https://cockpit-images.eu-central-1.linodeobjects.com/",
 )
 
 # hosted behind the Red Hat VPN

commit 981b189c918d9866ad0be8ecfe025799c30be0d9

Fix locally configured image stores
Commit 53f047eadad71 caused a regression: rhel* images were not uploaded
to the locally configured stores any more, i.e. our minio S3 cache on
the PSI cluster. That causes a lot of excess Linode S3 traffic (~ 120 GB
per image refresh). We want to always upload images to the locally configured
stores, let's assume the admin knows what they are doing.

That breakage wasn't obvious, as previously the locally configured stores were
just slapped onto `PUBLIC_STORES`. Make this more explicit by putting them into
`LOCAL_STORES` instead.

In return, drop `REDHAT_STORES`. It has been empty for a long time, and we
don't plan to re-add it any time soon.

Add unit tests for parsing the local store config.

image-download
@@ -46,8 +46,8 @@ from collections.abc import Iterator, Sequence
 from lib import s3
 from lib.constants import IMAGES_DIR
 from lib.directories import get_images_data_dir
-from lib.network import get_curl_ca_arg, redhat_network
-from lib.stores import PRIVATE_STORES, PUBLIC_STORES, REDHAT_STORES
+from lib.network import get_curl_ca_arg
+from lib.stores import LOCAL_STORES, PRIVATE_STORES, PUBLIC_STORES
 from lib.testmap import get_test_image
 
 EPOCH = "Thu, 1 Jan 1970 00:00:00 GMT"
@@ -136,10 +136,9 @@ def download(dest: str, force: bool, state: bool, quiet: bool, stores: Sequence[
         # Due to AI scraper-bot mitigation all public stores currently require
         # an access token: if we don't have one, then don't bother.
         stores = [
-            store for store in (*PUBLIC_STORES, *PRIVATE_STORES) if s3.is_key_present(urllib.parse.urlparse(store))
+            store for store in (*LOCAL_STORES, *PUBLIC_STORES, *PRIVATE_STORES)
+            if s3.is_key_present(urllib.parse.urlparse(store))
         ]
-        if redhat_network():
-            stores += REDHAT_STORES
         image_upload_store = os.environ.get('COCKPIT_IMAGE_UPLOAD_STORE')
         if image_upload_store:
             # NB: This is an *addition* to the built-in stores, used for

image-upload
@@ -28,8 +28,8 @@ import urllib.parse
 from lib import s3
 from lib.constants import BOTS_DIR, IMAGES_DIR
 from lib.directories import get_images_data_dir
-from lib.network import get_curl_ca_arg, redhat_network
-from lib.stores import PRIVATE_STORES, PUBLIC_STORES, REDHAT_STORES
+from lib.network import get_curl_ca_arg
+from lib.stores import LOCAL_STORES, PRIVATE_STORES, PUBLIC_STORES
 
 
 def upload(store: str, source: str, public: bool, prune: bool = False) -> bool:
@@ -105,14 +105,12 @@ def main() -> None:
                 # adds this to the list, if it's present.
                 stores = [image_upload_store]
         if not stores:
-            stores = list(PRIVATE_STORES)
+            stores = list(LOCAL_STORES) + list(PRIVATE_STORES)
             if public:
                 # NB: We currently have a well-known S3 token which is capable
                 # of accessing all data uploaded to these buckets, regardless
                 # of ACL.  Only upload public images here.
                 stores += PUBLIC_STORES
-            if redhat_network():
-                stores += REDHAT_STORES
         success = False
         for store in stores:
             success |= upload(store, source, public, prune=args.prune_s3)

lib/stores.py
@@ -29,21 +29,15 @@ PRIVATE_STORES: Sequence[str] = (
     "https://cockpit-images.eu-central-1.linodeobjects.com/",
 )
 
-# hosted behind the Red Hat VPN
-REDHAT_STORES: Sequence[str] = (
-    # e2e down for maintenance
-    # "https://cockpit-11.apps.cnfdb2.e2e.bos.redhat.com/images/",
-)
-
-# local stores
+# locally configured stores in ~/.config/cockpit-dev/image-stores or $COCKPIT_IMAGE_STORES_FILE
 try:
     with open(xdg_config_home('cockpit-dev', 'image-stores', envvar='COCKPIT_IMAGE_STORES_FILE')) as fp:
         data = fp.read().strip()
-        if data:
-            PUBLIC_STORES = (*PUBLIC_STORES, *data.split("\n"))
 except FileNotFoundError:
     # that config file is optional
-    pass
+    data = ""
+
+LOCAL_STORES: Sequence[str] = data.splitlines()
 
 
 LOG_STORE = "https://cockpit-logs.us-east-1.linodeobjects.com/"

test/test_stores.py
@@ -1,0 +1,54 @@
+# This file is part of Cockpit.
+#
+# Copyright (C) 2025 Red Hat, Inc.
+#
+# Cockpit is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# Cockpit is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+
+import importlib
+import os
+import unittest.mock
+from pathlib import Path
+
+import pytest
+
+
+@pytest.mark.parametrize("file_content,expected_stores", [
+    # configured stores
+    (
+        """https://example.com/store1/
+https://example.com/store2/
+
+""",
+        ["https://example.com/store1/", "https://example.com/store2/"]
+    ),
+    # empty config file
+    ("", []),
+    # whitespace-only
+    ("   \n   \n   ", []),
+    # absent config file
+    (None, [])
+])
+def test_local_stores(tmp_path: Path, file_content: str | None, expected_stores: list[str]) -> None:
+    """Test LOCAL_STORES with different config file scenarios."""
+    config_file = tmp_path / "image-stores"
+
+    if file_content is not None:
+        config_file.write_text(file_content)
+
+    with unittest.mock.patch.dict(os.environ, {"COCKPIT_IMAGE_STORES_FILE": str(config_file)}):
+        # Reload the module to pick up the new environment variable
+        import lib.stores
+        importlib.reload(lib.stores)
+
+        assert lib.stores.LOCAL_STORES == expected_stores