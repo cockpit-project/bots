6bd8a5e2c935aabe3fb54639551607d205693273
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2026-03-09 15:39:06 +0100

    lib: change allowlist from python to yaml
    This lets us read it directly from the GitHub API from Javascript GitHub
    actions, which we'll do in the next commit.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,4 +1,9 @@
-from .allowlist import ALLOWLIST
+from yaml import safe_load
+
+from .constants import LIB_DIR
+
+with open(f'{LIB_DIR}/allowlist.yaml') as f:
+    ALLOWLIST: set[str] = set(safe_load(f))
 
 __all__ = [
     'ALLOWLIST',
