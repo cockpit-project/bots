780ba7adab0f0bc4611fe4310eba05b2b9441f07
Jelle van der Waa <jvanderwaa@redhat.com>
2026-04-23 11:34:55 +0200

    testmap: move Cockpit to Ubuntu 2604, drop 2204
    "storage" and "networking" tests succeed now. "other" and "expensive"
    scenarios still have a few failures, let's enable them in a separate
    step.

--- lib/testmap.py
+++ lib/testmap.py
@@ -46,8 +46,9 @@ REPO_BRANCH_CONTEXT: Mapping[str, Mapping[str, Sequence[str]]] = {
             *contexts('centos-9-bootc', COCKPIT_SCENARIOS),
             *contexts('debian-testing', COCKPIT_SCENARIOS),
             *contexts('debian-trixie', COCKPIT_SCENARIOS),
-            *contexts('ubuntu-2204', COCKPIT_SCENARIOS),
             *contexts('ubuntu-2404', COCKPIT_SCENARIOS),
+            # FIXME: some remaining failures
+            *contexts('ubuntu-2604', COCKPIT_SCENARIOS - {'other', 'expensive'}),
             *contexts('ubuntu-stable', COCKPIT_SCENARIOS),
             *contexts('fedora-43', COCKPIT_SCENARIOS),
             *contexts('fedora-44', COCKPIT_SCENARIOS),
