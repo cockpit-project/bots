4ad143cbcb8035c713d89a339d5f1d01f3950aa6
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2026-05-29 09:15:09 +0200

    task: reduce shims to a minimum
    The only shim we want to keep is for old versions of `lcov.py` flying
    around (possibly as part of disto packages run during downstream
    testing).

--- task/__init__.py
+++ task/__init__.py
@@ -1,55 +1,7 @@
-# This file is part of Cockpit.
-#
-# Copyright (C) 2017 Red Hat, Inc.
-#
-# Cockpit is free software; you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 2.1 of the License, or
-# (at your option) any later version.
-#
-# Cockpit is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+"""Backwards compatibility shim.
+Older versions of lcov.py do 'from task import github'.
+"""
 
-"""Backwards compatibility shim - import from lib.task instead."""
+from lib import github
 
-from lib.task import (
-    api,
-    branch,
-    comment,
-    comment_done,
-    default_branch,
-    execute,
-    label,
-    labels_of_pull,
-    main,
-    pull,
-    push_branch,
-    run,
-    verbose,
-    would,
-)
-
-from . import github
-
-__all__ = (
-    "api",
-    "branch",
-    "comment",
-    "comment_done",
-    "default_branch",
-    "execute",
-    "github",
-    "label",
-    "labels_of_pull",
-    "main",
-    "pull",
-    "push_branch",
-    "run",
-    "verbose",
-    "would",
-)
+__all__ = ("github",)
